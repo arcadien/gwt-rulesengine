f5ee0eaf4caf877386b95499326c0bdc46cd8bfc
Aurélien Labrosse <aurelien.labrosse@gmail.com>
2012-06-27 01:16:56 +0200

    new unit test

---
+++ src/test/java/net/bobosse/gwt/rulesengine/client/impl/rules/AbstractRuleTest.java
@@ -0,0 +1,71 @@
+package net.bobosse.gwt.rulesengine.client.impl.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.bobosse.gwt.rulesengine.client.Report;
+import net.bobosse.gwt.rulesengine.client.Rule;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AbstractRuleTest {
+
+	private AbstractRule rule = createRule("default");
+
+	private static AbstractRule createRule(final String name) {
+		return new AbstractRule(name, 10) {
+
+			@Override
+			public void execute(Object fact, Report report) {
+				// TODO Auto-generated method stub
+			}
+		};
+	}
+
+	@Test
+	public void testSalience() {
+		Assert.assertEquals(10, rule.getSalience());
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testGetNullFact() {
+		rule.getFact();
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testGetNullReport() {
+		rule.getReport();
+	}
+
+	@Test
+	public void testActivationStatus() {
+
+		// rules are created active
+		junit.framework.Assert.assertEquals(true, rule.isActive());
+
+		// passivate rule
+		rule.passivate();
+		junit.framework.Assert.assertEquals(false, rule.isActive());
+
+		// activate rule
+		rule.activate();
+		junit.framework.Assert.assertEquals(true, rule.isActive());
+	}
+
+	@Test
+	public void testLinkedRules() {
+
+		List<Rule> rules = new ArrayList<Rule>();
+
+		rules.add(createRule("one"));
+		rules.add(createRule("two"));
+
+		rule.setFollowingRules(rules);
+		rule.setPreceedingRules(rules);
+
+		Assert.assertEquals(rules, rule.getFollowing());
+		Assert.assertEquals(rules, rule.getPreceeding());
+
+	}
+}
